924dd05c438271d77cfdddc7d2f9f898dd67725c
ThiagoErik55 <thiagoerik55@gmail.com>
2026-05-04 11:52:33 -0300

    first commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num, soma = 0; 
+        int maior = 0, menor = 0; 
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Insira um numero inteiro: ");
+            num = sc.nextInt();
+
+            soma += num;
+
+            if(i == 1){
+                maior = num;
+                menor = num;
+            } else {
+                if(num > maior){
+                    maior = num;
+                }
+                if(num < menor){
+                    menor = num;
+                }
+            }
+        }
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + (soma / 5));
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+    }
+}
