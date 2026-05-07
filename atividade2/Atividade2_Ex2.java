62aff67d6c32f190285b4b47f1a7ad04b2a6fde7
ThiagoErik55 <thiagoerik55@gmail.com>
2026-05-07 20:00:33 -0300

    exercise 2

--- atividade2/Atividade2_Ex2.java
+++ atividade2/Atividade2_Ex2.java
@@ -11,6 +11,8 @@ public class Atividade2_Ex2 {
 
         ArrayList<String> nomes = new ArrayList<>();
 
+        int opcao;
+
         nomes.add("João");
         nomes.add("Maria");
         nomes.add("Pedro");
@@ -18,8 +20,61 @@ public class Atividade2_Ex2 {
 
         Collections.sort(nomes);
 
-        for(String n : nomes) {
-            System.out.println(nome);
-        }
+        do{
+            System.out.print("\nDigite a opção desejada: \n");
+            System.out.println("1 - Adicionar nome");
+            System.out.println("2 - Remover nome");
+            System.out.println("3 - Buscar nome");
+            System.out.println("4 - Listar nomes");
+            System.out.println("0 - Sair");
+
+            opcao = sc.nextInt();
+
+            switch (opcao){
+
+                case 1:
+                    System.out.println("Digite o nome a ser adicionado: ");
+                    String adicionarNome = sc.next();
+                    nomes.add(adicionarNome);
+                    System.out.println("Nome adicionado!\n");
+                    break;
+
+                case 2:
+                    System.out.println("Digite o nome a ser removido: ");
+                    String removerNome = sc.next();
+                    if (nomes.remove(removerNome)){
+                        System.out.println("Nome removido!\n");
+                    } else {
+                        System.out.println("Nome não encontrado!\n");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Digite o nome a ser buscado: ");
+                    String buscarNome = sc.next();
+                    if (nomes.contains(buscarNome)){
+                        System.out.println("Nome encontrado!\n");
+                    } else {
+                        System.out.println("Nome não encontrado!\n");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Nomes cadastrados:");
+                    for (String nome : nomes) {
+                        System.out.println("- " + nome);
+                    }
+                    break;
+                
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida! Tente novamente.\n");
+            }
+
+        } while (opcao != 0);
+            sc.close();
     }
 }
